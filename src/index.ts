5f46bab696df9a8d2608394c507326df33a9d989
Baraka Larbi <larbibaraka@gmail.com>
2020-08-21 22:08:24 +0100

    Add Method to run migration at start

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import { MikroORM } from "@mikro-orm/core";
 import microConfig from "./mikro-orm.config";
 
 const main = async () => {
+  // connect to DataBase
   const orm = await MikroORM.init(microConfig);
+  // Run Migrations
+  await orm.getMigrator().up();
 };
 
 main();
