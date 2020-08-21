e415d1865bbc240bf632a93bfdadb55c4b021ef4
Baraka Larbi <larbibaraka@gmail.com>
2020-08-21 21:46:56 +0100

    Refactore the config of micro-orm  * the export as const is a great way but it does not treat the object as its real type which is MikroOrm.init  * Parameter is spicial thing in typeScript and it returns an array so we take the first parameter.

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,5 +1,6 @@
 import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
+import { MikroORM } from "@mikro-orm/core";
 
 export default {
   type: "postgresql",
@@ -8,4 +9,4 @@ export default {
   user: "postgres",
   password: "admin",
   debug: !__prod__,
-} as const;
+} as Parameters<typeof MikroORM.init>[0];
