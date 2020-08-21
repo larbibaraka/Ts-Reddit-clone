8d98610d9ef5f5000e41c2b7b52fda7d91ca09e7
Baraka Larbi <larbibaraka@gmail.com>
2020-08-21 21:33:06 +0100

    Add MikroOrm Config package.json

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
 
 const main = async () => {
-  const orm = await MikroORM.init({
+  await MikroORM.init({
     type: "postgresql",
+    entities: [Post],
     dbName: "reddit_clone",
     user: "postgres",
-    password: "",
+    password: "admin",
     debug: !__prod__,
   });
 };
