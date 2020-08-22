02f939dff7c754af8ef73453299b5d8dfe7971b8
Baraka Larbi <larbibaraka@gmail.com>
2020-08-22 10:27:39 +0100

    Initialize Graphql Server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import { MikroORM } from "@mikro-orm/core";
 import microConfig from "./mikro-orm.config";
 
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
 const main = async () => {
   // connect to DataBase
   const orm = await MikroORM.init(microConfig);
   // Run Migrations
   await orm.getMigrator().up();
+
+  // Create an express server
+  const app = express();
+  // Create Apollo Server
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [],
+      validate: false,
+    }),
+  });
+  app.listen(4000, () => {
+    console.log("Server is Running on localhost:4000");
+  });
 };
 
 main();
