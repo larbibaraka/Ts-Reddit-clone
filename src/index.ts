54a7de7eccd367e52225f150cbf4c3b19bf81dd3
Baraka Larbi <larbibaraka@gmail.com>
2020-08-22 10:56:46 +0100

    Pass context via context object

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   // connect to DataBase
@@ -17,9 +18,11 @@ const main = async () => {
   // Create Apollo Server
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    // context is a way to let us share orm.em object that we will need later on
+    context: () => ({ em: orm.em }),
   });
   // Create graphql end point on express
   apolloServer.applyMiddleware({ app });
