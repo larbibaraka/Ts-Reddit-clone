7d633b4936af6b9287687e7de0ac512f45c3b447
Baraka Larbi <larbibaraka@gmail.com>
2020-08-22 10:57:37 +0100

    Hook mikro-orm & Grphql to create the first query

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,10 +1,11 @@
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, Ctx } from "type-graphql";
 import { Post } from "./../entities/Post";
+import { MyContext } from "../types";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts() {
-    return [];
+  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+    return em.find(Post, {});
   }
 }
