dbc6e2fc93c923efcdd4686436f7b1bf49dd488e
Baraka Larbi <larbibaraka@gmail.com>
2020-08-29 23:51:34 +0100

    Add Create a new Post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,19 +1,30 @@
-import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
 import { Post } from "./../entities/Post";
 import { MyContext } from "../types";
 
 @Resolver()
 export class PostResolver {
+  /** Reading Area using Query*/
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
   }
 
-  @Query(() => Post, {nullable : true})
+  @Query(() => Post, { nullable: true })
   post(
-    @Arg("id" , ()=> Int) id : number,
-    @Ctx() {em} : MyContext
-    ): Promise<Post | null>{
-      return em.findOne(Post, {id})
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+  /** Create / Update / Delete using Mutation*/
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title", () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
   }
 }
