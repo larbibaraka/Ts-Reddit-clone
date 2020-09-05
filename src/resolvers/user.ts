8c4e721a6d5221e580665c4473fd403ad277558c
Baraka Larbi <larbibaraka@gmail.com>
2020-09-05 19:00:55 +0100

    Create login Function

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,8 +1,33 @@
-import { Resolver, Query, Mutation, Arg, Ctx } from "type-graphql";
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  Ctx,
+  ObjectType,
+  Field,
+} from "type-graphql";
 import { MyContext } from "../types";
 import { User } from "./../entities/User";
 
 import * as argon2 from "argon2";
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
 
 @Resolver()
 export class UserResolver {
@@ -11,7 +36,7 @@ export class UserResolver {
     @Arg("username", () => String) username: string,
     @Arg("password", () => String) password: string,
     @Ctx() { em }: MyContext
-  ) : Promise<User> {
+  ): Promise<User> {
     const hashedPassword = await argon2.hash(password);
     const user = await em.create(User, {
       username: username,
@@ -19,5 +44,41 @@ export class UserResolver {
     });
     await em.persistAndFlush(user);
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("username", () => String) username: string,
+    @Arg("password", () => String) password: string,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "username does not exist",
+          },
+        ],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, password);
+
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Invalid password",
+          },
+        ],
+      };
+    }
+
+    return {
+      user,
+    };
   }
 }
