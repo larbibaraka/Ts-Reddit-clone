fc5da14f3ddace90ea8652e4e83a4160019e7a33
Baraka Larbi <larbibaraka@gmail.com>
2020-09-05 16:14:48 +0100

    Create Register Mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,23 @@
-import { Resolver } from "type-graphql";
+import { Resolver, Query, Mutation, Arg, Ctx } from "type-graphql";
+import { MyContext } from "../types";
+import { User } from "./../entities/User";
+
+import * as argon2 from "argon2";
 
 @Resolver()
-export class UserResolver  {
-
+export class UserResolver {
+  @Mutation(() => User)
+  async register(
+    @Arg("username", () => String) username: string,
+    @Arg("password", () => String) password: string,
+    @Ctx() { em }: MyContext
+  ) : Promise<User> {
+    const hashedPassword = await argon2.hash(password);
+    const user = await em.create(User, {
+      username: username,
+      password: hashedPassword,
+    });
+    await em.persistAndFlush(user);
+    return user;
+  }
 }
